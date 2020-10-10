85be41cea4101bda38de5ce02bfb76308e407140
zhuyaliang <15132211195@163.com>
2020-10-10 17:25:28 +0800

    use GDK_WINDOW_TYPE_HINT_MENU window type

--- menu-window.c
+++ menu-window.c
@@ -98,7 +98,7 @@ menu_window_init (MenuWindow *menuwin)
     menuwin->priv = menu_window_get_instance_private (menuwin);
 
     window = GTK_WINDOW (menuwin);
-    gtk_window_set_type_hint (window, GDK_WINDOW_TYPE_HINT_DOCK);
+    gtk_window_set_type_hint (window, GDK_WINDOW_TYPE_HINT_MENU);
     gtk_window_set_decorated (window, FALSE);
     gtk_window_set_resizable (window, FALSE);
     gtk_window_stick (window);
